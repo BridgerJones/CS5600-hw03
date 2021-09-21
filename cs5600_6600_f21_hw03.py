ca68bbc5577a3a4d4fc6916cac24fbfed9733038
Bridger Jones <bridgerjones15@gmail.com>
2021-09-21 12:38:27 -0600

    traning 2 works but took for freakin ever

--- cs5600_6600_f21_hw03.py
+++ cs5600_6600_f21_hw03.py
@@ -9,18 +9,22 @@ train_d, valid_d, test_d = load_data_wrapper()
 HLS = [10, 25, 50]
 ETA = [0.5, 0.25, 0.125]
 def train_1_hidden_layer_anns(hls=HLS, eta=ETA, mini_batch_size=10, num_epochs=10):
-    %%time
     for h in hls:
         for n in eta:
             network = ann([784, h, 10])
             print(f"*** Training 784x{h}x10 ANN with eta={n}")
             network.mini_batch_sgd(train_d, num_epochs, mini_batch_size, n, test_data=test_d)
             print(f"*** Training 784x{h}x10 ANN DONE...\n\n")
-# def train_2_hidden_layer_anns(hls=ETA, eta=ETA, mini_batch_size=10, num_epochs=10):
-#     ### your code here
-#     pass
+def train_2_hidden_layer_anns(hls=ETA, eta=ETA, mini_batch_size=10, num_epochs=10):
+    for h in hls:
+        for h_2 in hls:
+            for n in eta:
+                network = ann([784, h, h_2, 10])
+                print(f"*** Training 784x{h}x{h_2}x10 ANN with eta={n}")
+                network.mini_batch_sgd(train_d, num_epochs, mini_batch_size, n, test_data=test_d)
+                print(f"*** Training 784x{h}x{h_2}x10 ANN DONE...\n\n")
 
 ### Uncomment to run
 if __name__ == '__main__':
-    train_1_hidden_layer_anns(hls=HLS, eta=ETA, mini_batch_size=10, num_epochs=10)
-    #train_2_hidden_layer_anns(hls=HLS, eta=ETA, mini_batch_size=10, num_epochs=10)
+    #train_1_hidden_layer_anns(hls=HLS, eta=ETA, mini_batch_size=10, num_epochs=10)
+    train_2_hidden_layer_anns(hls=HLS, eta=ETA, mini_batch_size=10, num_epochs=10)
